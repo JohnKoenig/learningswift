c797f8f100fabdf17a483245159f04d4bb3bfdcc
koenigj3@gmail.com <jkoenig@kemi.com>
2020-02-22 16:24:43 -0500

    test

---
+++ arrayExample.swift
@@ -0,0 +1,4 @@
+var shoppingList = ["catfish","water","tulips"]
+print(shoppingList[1])
+shoppingList.append("blue paint")
+pring(shoppingList)
---
+++ dictionaryExample.swift
@@ -0,0 +1,5 @@
+var occupations = [
+    "Malcom": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations ["Jayne"] = "Public Relations"
